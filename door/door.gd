f775217295ea86e08bb789228cbdb252299dd4f4
Aidan Rhoden <70079907+aidan-j-rhoden@users.noreply.github.com>
2022-08-24 12:23:44 -0500

    Added player detection for door

--- door/door.gd
+++ door/door.gd
@@ -8,3 +8,9 @@ func _on_door_body_entered(body):
 	if not open and body is Player:
 		animation_player.play("doorsimple_opening")
 		open = true
+
+
+func _on_door_body_exited(body):
+	if open and body is Player:
+		animation_player.play("doorsimple_closing")
+		open = false
