0dcaf6ed11bf4bf1aaff933825a9ca5eb3e5b9a9
aidan-j-rhoden <aidan.j.rhoden@gmail.com>
2022-08-25 11:27:08 -0500

    Added a door to core.tscn

--- door/door.gd
+++ door/door.gd
@@ -1,12 +1,14 @@
 extends Area
 
 var open = false
+var hasBeenOpened = false
 
 onready var animation_player = $DoorModel/AnimationPlayer
 
 func _on_door_body_entered(body):
-	if not open and body is Player:
+	if not open and body is Player or not open and hasBeenOpened:
 		animation_player.play("doorsimple_opening")
+		hasBeenOpened = true
 		open = true
 
 
