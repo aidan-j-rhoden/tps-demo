c42146e236a1b90309a55c56a9866d6805137796
aidan-j-rhoden <aidan.j.rhoden@gmail.com>
2022-08-25 12:02:16 -0500

    Revert "Merged branch Doors (#2)"
    This reverts commit be0cc3c4afc3a71ec68a0ce0438335f5d45967aa.

--- door/door.gd
+++ door/door.gd
@@ -8,9 +8,3 @@ func _on_door_body_entered(body):
 	if not open and body is Player:
 		animation_player.play("doorsimple_opening")
 		open = true
-
-
-func _on_door_body_exited(body):
-	if open and body is Player:
-		animation_player.play("doorsimple_closing")
-		open = false
