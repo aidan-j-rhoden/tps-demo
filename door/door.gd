017bafd633662fcc6434843fcb807f2e804d1c9e
aidan-j-rhoden <aidan.j.rhoden@gmail.com>
2022-09-04 16:32:04 -0500

    Updated door.gd

--- door/door.gd
+++ door/door.gd
@@ -1,16 +1,11 @@
 extends Area
 
 var open = false
-var hasBeenOpened = false
 
 onready var animation_player = $DoorModel/AnimationPlayer
 
 func _on_door_body_entered(body):
 	if not open and body is Player:
-		animation_player.play("doorsimple_opening")
-		hasBeenOpened = true
-		open = true
-	elif not open and hasBeenOpened:
 		animation_player.play("doorsimple_opening")
 		open = true
 
